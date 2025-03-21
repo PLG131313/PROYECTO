2b8837214a1285709b1c4733606752ccd73022a4
PLG131313 <154220770+PLG131313@users.noreply.github.com>
2025-03-21 08:34:41 +0100

    Update conexion_mail.php

--- php/conexion_mail.php
+++ php/conexion_mail.php
@@ -5,7 +5,7 @@ $Username="cuentapruebaplg1313@gmail.com";
 $Password ="jzur qtov wfrj bjvx";
 
 // este correo que pongas aquí lo visualizará el destinatario
-$From="cuentapruebaplg1313@gmail.com";
+$From="pablopezgomez13@gmail.com";
 
 // tu nombre real
 $FromName="Pablo";
